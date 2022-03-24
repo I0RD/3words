901493feacab4ad89b4b45e48043fba5d8878432
I0RD <michallyszkowski5@gmail.com>
2022-03-24 09:30:23 +0100

    final

--- AdvancedWindow.java
+++ AdvancedWindow.java
@@ -7,9 +7,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
-public class AdvancedWindow  {
+public class AdvancedWindow
+{
 
     public  JPanel panel;
     private  JButton getByWordsButton;
@@ -23,18 +25,13 @@ public class AdvancedWindow  {
     private JButton RandomLatLongButton;
     private  String[] str;
 
-    private void createUIComponents()
-    {
-
-    }
-
     public AdvancedWindow()
     {
         getByLatLongButton.addActionListener(event->get3Words());
         getByWordsButton.addActionListener(event->getWorld());
         RandomLatLongButton.addActionListener(event-> {
             try {
-                losuj();
+                randomLatLong();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -43,34 +40,33 @@ public class AdvancedWindow  {
 
     public static void main(String[] args)
     {
-       JFrame jFrame=new JFrame("Okno");
+       JFrame jFrame=new JFrame("3Words API");
        jFrame.setContentPane(new AdvancedWindow().panel);
        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        jFrame.pack();
        jFrame.setVisible(true);
-
     }
     public void get3Words() {
         try {
             String longitude=longitudeText.getText();
             String latitude=latitudeText.getText();
-            String spec="https://api.what3words.com/v3/convert-to-3wa?"+
-                    "coordinates="+latitude+"%2C"+longitude+"&key=F8QT0PCM";
-
+            String spec;
             if(plCheckBox.isSelected())
             {
-                spec+="&language=pl";
+                spec="https://api.what3words.com/v3/convert-to-3wa?"+ "coordinates="+latitude+"%2C"+longitude+"&language=pl&key=F8QT0PCM";
+            }
+            else
+            {
+                spec="https://api.what3words.com/v3/convert-to-3wa?"+ "coordinates="+latitude+"%2C"+longitude+"&key=F8QT0PCM";
             }
             URL url = new URL(spec);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
+
+            if (conn.getResponseCode() != 200) {throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());}
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
             String output;
             String resault="";
             System.out.println("Output from Server .... \n");
@@ -85,28 +81,23 @@ public class AdvancedWindow  {
             wordsurl=wordsurl.replaceAll("\\\\","");
             goWebsite(UrlLabel,wordsurl,wordsurl);
             wordsTextField.setText(words);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (Exception e) {e.printStackTrace();}
     }
     private  void getWorld()
     {
         try {
             String words=wordsTextField.getText();
-            words= URLEncoder.encode(words,"UTF-8");
-            String spec="https://api.what3words.com/v3/convert-to-coordinates?"+
-                    "words="+words+"&key=F8QT0PCM";
+            words= URLEncoder.encode(words, StandardCharsets.UTF_8);
+            String spec="https://api.what3words.com/v3/convert-to-coordinates?"+ "words="+words+"&key=F8QT0PCM";
             URL url = new URL(spec);
             System.out.println(spec);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
+
+            if (conn.getResponseCode() != 200) {throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());}
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
             String output;
             String resault="";
             System.out.println("Output from Server .... \n");
@@ -123,38 +114,38 @@ public class AdvancedWindow  {
             latitudeText.setText(lat);
             longitudeText.setText(lon);
             conn.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (Exception e) {e.printStackTrace();}
     }
-    private void losuj() throws IOException {
+    private void randomLatLong() throws IOException {
         try
         {
         int startLon=14;
-        int endLon=23;
+        int endLon=24;
         int startLat=49;
-        double endLat=54.5;
+        double endLat=55;
         double randLon=new Random().nextDouble();
-        double randLat=new Random().nextDouble();;
-        double randomLon=startLon+(randLon*(endLon-randLon));
-        double randomLat=startLat+(randLat*(endLat-randLat));
+        double randLat=new Random().nextDouble();
+        double randomLon=startLon+((randLon*(endLon-randLon))/10);
+        double randomLat=startLat+((randLat*(endLat-randLat))/10);
         latitudeText.setText(""+randomLat);
         longitudeText.setText(""+randomLon);
-        String spec="https://api.what3words.com/v3/convert-to-3wa?"+
-                "coordinates="+randLon+"%2C"+randLat+"&key=F8QT0PCM";
+        String spec;
             if(plCheckBox.isSelected())
             {
-                spec+="&language=pl";
-            }        URL url = new URL(spec);
+                spec="https://api.what3words.com/v3/convert-to-3wa?"+ "coordinates="+randomLat+"%2C"+randomLon+"&language=pl&key=F8QT0PCM";
+            }
+            else
+            {
+                spec="https://api.what3words.com/v3/convert-to-3wa?"+ "coordinates="+randomLat+"%2C"+randomLon+"&key=F8QT0PCM";
+            }
+            URL url = new URL(spec);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Accept", "application/json");
         if (conn.getResponseCode() != 200) {
-            throw new RuntimeException("Failed : HTTP error code : "
-                    + conn.getResponseCode());
+            throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
         }
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                (conn.getInputStream())));
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
         String output;
         String resault="";
         System.out.println("Output from Server .... \n");
@@ -171,26 +162,24 @@ public class AdvancedWindow  {
         wordsTextField.setText(words);
     }
         catch (Exception e)
-        {
-        e.printStackTrace();
-        }
+        {e.printStackTrace();}
     }
     private void goWebsite(JLabel website, final String url, String text)
     {
         website.setText("<html> Website : <a href=\"\">"+text+"</a></html>");
         website.setCursor(new Cursor(Cursor.HAND_CURSOR));
-       for(MouseListener ms:website.getMouseListeners()) {
-        website.removeMouseListener(ms);
-       }
-           website.addMouseListener(new MouseAdapter() {
+       for(MouseListener ms:website.getMouseListeners())
+       {website.removeMouseListener(ms);}
+           website.addMouseListener(new MouseAdapter()
+           {
                @Override
-               public void mouseClicked(MouseEvent e) {
-                   try {
+               public void mouseClicked(MouseEvent e)
+               {
+                   try
+                   {
                        Desktop.getDesktop().browse(new URI(url));
-                   } catch (URISyntaxException | IOException ex) {
-                       ex.printStackTrace();
-                   }
+                   } catch (URISyntaxException | IOException ex) {ex.printStackTrace();}
                }
            });
-       }
     }
+}
